694958a84b829b18e3f07932c518ffa0bb161061
Ting97ok <89396424+Ting97ok@users.noreply.github.com>
2024-11-23 18:05:14 +0900

    [Feat] KAN-230 회원가입 API (#10)

--- src/main/kotlin/com/restaurant/be/user/domain/entity/User.kt
+++ src/main/kotlin/com/restaurant/be/user/domain/entity/User.kt
@@ -3,6 +3,7 @@ package com.restaurant.be.user.domain.entity
 import com.restaurant.be.common.converter.SeparatorConverter
 import com.restaurant.be.common.entity.BaseEntity
 import com.restaurant.be.user.domain.constant.Gender
+import com.restaurant.be.user.presentation.dto.SignUpUserRequest
 import jakarta.persistence.Column
 import jakarta.persistence.Convert
 import jakarta.persistence.Entity
@@ -42,6 +43,19 @@ class User(
     @OneToMany(mappedBy = "user", fetch = LAZY)
     var socialUsers: MutableList<SocialUser> = mutableListOf()
 ) : BaseEntity() {
+    companion object {
+        fun create(request: SignUpUserRequest, nickname: String): User {
+            return User(
+                phoneNumber = request.phoneNumber,
+                nickname = nickname,
+                name = request.name,
+                birthday = request.birthday,
+                gender = request.gender,
+                isTermsAgreed = true
+            )
+        }
+    }
+
     fun delete() {
         this.isDeleted = true
     }
