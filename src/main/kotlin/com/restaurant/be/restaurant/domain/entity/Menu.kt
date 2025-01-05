6b9ef54027cc7b8a0622f773af80ca1c5ad3698b
lsc713 <lsc7134@naver.com>
2025-01-06 02:18:59 +0900

    chore : [KAN-288] ./gradlew ktlinFormat  적용

--- src/main/kotlin/com/restaurant/be/restaurant/domain/entity/Menu.kt
+++ src/main/kotlin/com/restaurant/be/restaurant/domain/entity/Menu.kt
@@ -1,7 +1,12 @@
 package com.restaurant.be.restaurant.domain.entity
 
 import com.restaurant.be.restaurant.presentation.controller.dto.common.MenuDto
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "menus")
