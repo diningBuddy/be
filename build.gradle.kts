69061d764c00347c6ec6216a89f7f962fb275318
lsc713 <lsc7134@naver.com>
2025-01-08 03:13:45 +0900

    docu : [KAN-276] dependency수정 data.elasticsearch추가 주입

--- build.gradle.kts
+++ build.gradle.kts
@@ -103,7 +103,7 @@ dependencies {
     testImplementation("org.testcontainers:elasticsearch:1.16.2")
 
     // ES
-    testImplementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
+    implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
 }
 
 allOpen {
