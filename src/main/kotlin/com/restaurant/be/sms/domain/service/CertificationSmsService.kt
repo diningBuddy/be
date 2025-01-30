b4866c8d053781625fad70caab8e3e8b79ba6081
sinkyoungdeok <tkagmd1@naver.com>
2025-01-30 10:28:44 +0900

    [KAN-000] 인증번호 요청을 임시로 고정값으로 변경

--- src/main/kotlin/com/restaurant/be/sms/domain/service/CertificationSmsService.kt
+++ src/main/kotlin/com/restaurant/be/sms/domain/service/CertificationSmsService.kt
@@ -12,17 +12,18 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional(readOnly = true)
 class CertificationSmsService(
     private val smsRepository: SmsRepository,
-    private val redisRepository: RedisRepository
+    private val redisRepository: RedisRepository,
 ) {
-
     fun sendCertificationNumber(request: SendCertificationSmsRequest) {
-        val certificationNumber = (1000..9999).random()
-        val sendMessage = """
-            [다이닝버디]
-            인증번호 : $certificationNumber
-            """
+//        val certificationNumber = (1000..9999).random()
+//        val sendMessage = """
+//            [다이닝버디]
+//            인증번호 : $certificationNumber
+//            """
+        // 당분간은 인증번호 고정
+        val certificationNumber = 1111
         redisRepository.saveCertificationNumber(request.phoneNumber, certificationNumber)
-        smsRepository.sendSms(request.phoneNumber, sendMessage)
+//        smsRepository.sendSms(request.phoneNumber, sendMessage)
     }
 
     fun verifyCertificationNumber(request: VerifyCertificationSmsRequest) {
