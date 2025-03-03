f6c58594b2241f022919b4dff84222fb05692e33
lsc713 <lsc7134@naver.com>
2025-03-04 02:14:51 +0900

    Chore : [KAN-321] restaurantId 저장

--- src/main/kotlin/com/restaurant/be/restaurant/domain/entity/RestaurantRank.kt
+++ src/main/kotlin/com/restaurant/be/restaurant/domain/entity/RestaurantRank.kt
@@ -2,12 +2,9 @@ package com.restaurant.be.restaurant.domain.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 
 @Entity
@@ -17,9 +14,8 @@ class RestaurantRank(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long,
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "restaurant_id", nullable = false)
-    var restaurant: Restaurant,
+    @Column(name = "restaurant_id", nullable = false)
+    var restaurantId: Long,
 
     @Column(name = "rank_number", nullable = false)
     var rank: Int
