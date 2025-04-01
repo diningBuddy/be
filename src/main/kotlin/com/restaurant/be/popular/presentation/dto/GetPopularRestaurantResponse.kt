3837d350886eebdcff528eae47cdc55903c858d6
lsc713 <lsc7134@naver.com>
2025-04-02 01:26:58 +0900

    :fire: 사용하지 않는 클래스들을 제거하다

--- src/main/kotlin/com/restaurant/be/popular/presentation/dto/GetPopularRestaurantResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.restaurant.be.popular.presentation.dto
-
-import com.restaurant.be.restaurant.presentation.controller.dto.common.PopularRestaurantDto
-import io.swagger.v3.oas.annotations.media.Schema
-
-data class GetPopularRestaurantResponse(
-    @Schema(description = "인기 음식점 리스트")
-    val popularRestaurants: List<PopularRestaurantDto>
-)
