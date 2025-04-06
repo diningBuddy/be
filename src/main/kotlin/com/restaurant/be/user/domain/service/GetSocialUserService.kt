677d98f4d2a1c37f68aa3d37160f1547b8c4ef8b
xjwl0305 <xjwl0305@gmail.com>
2025-04-06 17:27:11 +0900

    ADD getSocialUser API

---
+++ src/main/kotlin/com/restaurant/be/user/domain/service/GetSocialUserService.kt
@@ -0,0 +1,22 @@
+package com.restaurant.be.user.domain.service
+
+import com.restaurant.be.common.exception.DuplicateUserNicknameException
+import com.restaurant.be.common.exception.NotFoundUserException
+import com.restaurant.be.user.presentation.dto.CheckNicknameResponse
+import com.restaurant.be.user.presentation.dto.GetSocialUserDtoResponse
+import com.restaurant.be.user.repository.UserRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+
+@Service
+class GetSocialUserService(
+    private val userRepository: UserRepository
+) {
+    fun getSocialUser(userId: Long): GetSocialUserDtoResponse {
+        val user = userRepository.findByIdOrNull(userId) ?: throw NotFoundUserException()
+        return GetSocialUserDtoResponse(
+            id = userId, socialType = user.socialUsers.firstOrNull()?.socialType,
+            socialKey = user.socialUsers.firstOrNull()?.socialKey ?: ""
+        )
+    }
+}
--- src/main/kotlin/com/restaurant/be/user/repository/UserRepository.kt
+++ src/main/kotlin/com/restaurant/be/user/repository/UserRepository.kt
@@ -2,6 +2,8 @@ package com.restaurant.be.user.repository
 
 import com.restaurant.be.user.domain.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface UserRepository : JpaRepository<User, Long> {
     fun findByPhoneNumber(phoneNumber: String): User?
