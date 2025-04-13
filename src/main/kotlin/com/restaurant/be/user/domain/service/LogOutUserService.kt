68773b4e98817292bd512780137b00a94c7bb038
xjwl0305 <xjwl0305@gmail.com>
2025-04-13 20:49:00 +0900

    FIX clean project by ktlint

--- src/main/kotlin/com/restaurant/be/user/domain/service/LogOutUserService.kt
+++ src/main/kotlin/com/restaurant/be/user/domain/service/LogOutUserService.kt
@@ -1,13 +1,8 @@
 package com.restaurant.be.user.domain.service
 
 import com.restaurant.be.common.exception.NotFoundUserException
-import com.restaurant.be.common.jwt.TokenProvider
 import com.restaurant.be.common.redis.RedisRepository
-import com.restaurant.be.common.response.Token
-import com.restaurant.be.sms.domain.service.CertificationSmsService
-import com.restaurant.be.sms.presentation.dto.VerifyCertificationSmsRequest
 import com.restaurant.be.user.presentation.dto.LogOutUserRequest
-import com.restaurant.be.user.presentation.dto.SignInUserRequest
 import com.restaurant.be.user.repository.UserRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -16,14 +11,14 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional(readOnly = true)
 class LogOutUserService(
     private val userRepository: UserRepository,
-    private val redisRepository: RedisRepository
+    private val redisRepository: RedisRepository,
 ) {
     fun logout(request: LogOutUserRequest): Boolean {
-        val user = userRepository.findById(request.id).orElseThrow {
-            NotFoundUserException()
-        }
+        val user =
+            userRepository.findById(request.id).orElseThrow {
+                NotFoundUserException()
+            }
         redisRepository.deleteRefreshToken(request.id)
         return true
     }
-
 }
