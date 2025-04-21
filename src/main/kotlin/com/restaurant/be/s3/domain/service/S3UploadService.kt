fd7487be9d65675f6dafaf70ff0cbfa9472e673e
lsc713 <lsc7134@naver.com>
2025-04-22 01:16:47 +0900

    [KAN-351] lint

--- src/main/kotlin/com/restaurant/be/s3/domain/service/S3UploadService.kt
+++ src/main/kotlin/com/restaurant/be/s3/domain/service/S3UploadService.kt
@@ -2,14 +2,17 @@ package com.restaurant.be.s3.domain.service
 
 import com.restaurant.be.common.config.S3Properties
 import com.restaurant.be.s3.util.ImageResizeUtil
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
 import java.time.LocalDate
-import java.util.*
+import java.util.UUID
 
 @Service
 class S3UploadService(
