df88cb48e05789048c52cfbe70030e34856458e1
lsc713 <lsc7134@naver.com>
2025-04-21 22:10:43 +0900

    [KAN-351] 비동기 업로드와 성능 차이를 테스트를 통해 확인

--- src/main/kotlin/com/restaurant/be/s3/domain/service/S3UploadService.kt
+++ src/main/kotlin/com/restaurant/be/s3/domain/service/S3UploadService.kt
@@ -2,11 +2,15 @@ package com.restaurant.be.s3.domain.service
 
 import com.restaurant.be.common.config.S3Properties
 import com.restaurant.be.s3.util.ImageResizeUtil
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
+import org.springframework.web.servlet.function.ServerResponse.async
 import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.services.s3.S3Client
-import software.amazon.awssdk.services.s3.model.ObjectCannedACL
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
 import java.time.LocalDate
 import java.util.*
@@ -16,6 +20,18 @@ class S3UploadService(
     private val s3Client: S3Client,
     private val s3Properties: S3Properties
 ) {
+    fun uploadImagesSync(files: List<MultipartFile>): List<String> {
+        return files.map { uploadImage(it) }
+    }
+
+    suspend fun uploadImagesAsync(files: List<MultipartFile>): List<String> = coroutineScope {
+        files.map { file ->
+            async(Dispatchers.IO) {
+                uploadImage(file)
+            }
+        }.awaitAll()
+    }
+
     fun uploadImage(file: MultipartFile): String {
         val resizedImageBytes = ImageResizeUtil.resizeAndCompressWithFallback(file, 1280)
         val uuid = UUID.randomUUID().toString()
--- src/main/kotlin/com/restaurant/be/s3/presentation/ImageUploadController.kt
+++ src/main/kotlin/com/restaurant/be/s3/presentation/ImageUploadController.kt
@@ -6,13 +6,17 @@ import io.swagger.v3.oas.annotations.media.ArraySchema
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
+import kotlinx.coroutines.runBlocking
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
+import java.security.Principal
+import kotlin.system.measureTimeMillis
 
 @RestController
 @RequestMapping("/v1")
@@ -21,22 +25,62 @@ class ImageUploadController(
 ) {
 
     @Operation(
-        summary = "다중 이미지 업로드",
+        summary = "다중 이미지 업로드 동기",
         description = "이미지 파일을 여러 개 업로드하고, 업로드된 이미지의 URL 리스트를 반환합니다.",
         responses = [
-            ApiResponse(responseCode = "200", description = "성공", content = [
-                Content(array = ArraySchema(schema = Schema(implementation = String::class)))
-            ]),
+            ApiResponse(
+                responseCode = "200",
+                description = "성공",
+                content = [
+                    Content(array = ArraySchema(schema = Schema(implementation = String::class)))
+                ]
+            ),
             ApiResponse(responseCode = "400", description = "잘못된 요청 (파일이 없음)"),
             ApiResponse(responseCode = "500", description = "서버 에러")
         ]
     )
-    @PostMapping("/upload-image", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
-    fun uploadImages(
+    @PostMapping("/upload-image/sync", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    @PreAuthorize("hasRole('USER')")
+    fun uploadImagesSync(
+        principal: Principal,
         @RequestPart files: List<MultipartFile>
     ): ResponseEntity<List<String>> {
-        if (files.isEmpty()) return ResponseEntity.badRequest().build()
-        val urls = files.map { uploadService.uploadImage(it) }
+        var urls: List<String>
+        val time = measureTimeMillis {
+            urls = uploadService.uploadImagesSync(files)
+        }
+        println("🧱 동기 업로드 소요 시간: ${time}ms")
+        return ResponseEntity.ok(urls)
+    }
+
+    @Operation(
+        summary = "다중 이미지 업로드 비동기",
+        description = "이미지 파일을 여러 개 업로드하고, 업로드된 이미지의 URL 리스트를 반환합니다.",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "성공",
+                content = [
+                    Content(array = ArraySchema(schema = Schema(implementation = String::class)))
+                ]
+            ),
+            ApiResponse(responseCode = "400", description = "잘못된 요청 (파일이 없음)"),
+            ApiResponse(responseCode = "500", description = "서버 에러")
+        ]
+    )
+    @PostMapping("/upload-image/async", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    @PreAuthorize("hasRole('USER')")
+    suspend fun uploadImagesAsync(
+        principal: Principal,
+        @RequestPart files: List<MultipartFile>
+    ): ResponseEntity<List<String>> {
+        var urls: List<String>
+        val time = measureTimeMillis {
+            urls = runBlocking {
+                uploadService.uploadImagesAsync(files)
+            }
+        }
+        println("⚡ 비동기(코루틴) 업로드 소요 시간: ${time}ms")
         return ResponseEntity.ok(urls)
     }
 }
