bc3092031685c5d1951fb15a6bc8ba56f4f5c3dc
lsc713 <lsc7134@naver.com>
2025-04-24 02:30:54 +0900

    [KAN-351] 업로드 경로별 폴더 분리

--- src/main/kotlin/com/restaurant/be/s3/domain/UploadPath.kt
+++ src/main/kotlin/com/restaurant/be/s3/domain/UploadPath.kt
@@ -2,6 +2,7 @@ package com.restaurant.be.s3.domain
 
 enum class UploadPath(val folderName: String) {
     USER_PROFILE("user-profile"),
-    REVIEW("review")
+    REVIEW("review"),
+    EDIT_INQUIRE("edit-inquire")
     ;
 }
