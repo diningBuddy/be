5a25dab8e0a009fd26f42bc554868fad945af2fa
lsc713 <lsc7134@naver.com>
2025-04-23 09:59:17 +0900

    [KAN-351] 동기 업로드 제거

--- src/main/kotlin/com/restaurant/be/s3/presentation/ImageUploadController.kt
+++ src/main/kotlin/com/restaurant/be/s3/presentation/ImageUploadController.kt
@@ -23,30 +23,6 @@ class ImageUploadController(
 ) {
 
     @Operation(
-        summary = "다중 이미지 업로드 동기",
-        description = "이미지 파일을 여러 개 업로드하고, 업로드된 이미지의 URL 리스트를 반환합니다.",
-        responses = [
-            ApiResponse(
-                responseCode = "200",
-                description = "성공",
-                content = [
-                    Content(array = ArraySchema(schema = Schema(implementation = String::class)))
-                ]
-            ),
-            ApiResponse(responseCode = "400", description = "잘못된 요청 (파일이 없음)"),
-            ApiResponse(responseCode = "500", description = "서버 에러")
-        ]
-    )
-    @PostMapping("/upload-image/sync", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
-    @PreAuthorize("hasRole('USER')")
-    fun uploadImagesSync(
-        principal: Principal,
-        @RequestPart files: List<MultipartFile>
-    ): CommonResponse<List<String>> {
-        return CommonResponse.success(uploadService.uploadImagesSync(files))
-    }
-
-    @Operation(
         summary = "다중 이미지 업로드 비동기",
         description = "이미지 파일을 여러 개 업로드하고, 업로드된 이미지의 URL 리스트를 반환합니다.",
         responses = [
