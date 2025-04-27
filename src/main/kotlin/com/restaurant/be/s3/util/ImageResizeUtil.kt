d45b37a8b00d84eeccdbbc323002f236ca84a61a
이주호 <139448668+lsc713@users.noreply.github.com>
2025-04-27 12:10:25 +0900

    Kan 351 다중 이미지 업로드 추가 (#56)

---
+++ src/main/kotlin/com/restaurant/be/s3/util/ImageResizeUtil.kt
@@ -0,0 +1,66 @@
+package com.restaurant.be.s3.util
+
+import net.coobird.thumbnailator.Thumbnails
+import org.springframework.web.multipart.MultipartFile
+import java.awt.image.BufferedImage
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import javax.imageio.ImageIO
+
+object ImageResizeUtil {
+
+    fun resizeAndCompressWithFallback(file: MultipartFile, maxWidth: Int = 1280): ByteArray {
+        val originalImage = ImageIO.read(file.inputStream)
+
+        if (originalImage == null) {
+            throw IllegalArgumentException("이미지를 읽을 수 없습니다.")
+        }
+
+        val width = originalImage.width
+        val height = originalImage.height
+
+        if (width <= maxWidth && height <= maxWidth) {
+            return compressImage(file.bytes, width, height)
+        }
+
+        val resizedBytes = resizeImage(originalImage, maxWidth)
+        val compressedBytes = compressImage(resizedBytes, maxWidth, maxWidth)
+
+        return if (compressedBytes.size < resizedBytes.size) {
+            compressedBytes
+        } else {
+            resizedBytes
+        }
+    }
+
+    private fun resizeImage(image: BufferedImage, maxWidth: Int): ByteArray {
+        val aspectRatio = image.width.toDouble() / image.height.toDouble()
+        val newWidth: Int
+        val newHeight: Int
+
+        if (image.width > image.height) {
+            newWidth = maxWidth
+            newHeight = (maxWidth / aspectRatio).toInt()
+        } else {
+            newHeight = maxWidth
+            newWidth = (maxWidth * aspectRatio).toInt()
+        }
+
+        val outputStream = ByteArrayOutputStream()
+        Thumbnails.of(image)
+            .size(newWidth, newHeight)
+            .outputFormat("jpg")
+            .toOutputStream(outputStream)
+        return outputStream.toByteArray()
+    }
+
+    private fun compressImage(inputBytes: ByteArray, width: Int, height: Int): ByteArray {
+        val outputStream = ByteArrayOutputStream()
+        Thumbnails.of(ByteArrayInputStream(inputBytes))
+            .size(width, height)
+            .outputFormat("jpg")
+            .outputQuality(0.8)
+            .toOutputStream(outputStream)
+        return outputStream.toByteArray()
+    }
+}
