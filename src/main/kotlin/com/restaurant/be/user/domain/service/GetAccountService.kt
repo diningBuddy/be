24756c4be03e0c7bf80eb9593308d8aa547743bf
xjwl0305 <xjwl0305@gmail.com>
2025-05-01 23:25:08 +0900

    ADD getAccountinfo func [KAN-331]

--- src/main/kotlin/com/restaurant/be/user/domain/service/GetAccountService.kt
+++ src/main/kotlin/com/restaurant/be/user/domain/service/GetAccountService.kt
@@ -1,4 +1,20 @@
 package com.restaurant.be.user.domain.service
 
-class GetAccountService {
+import com.restaurant.be.common.exception.NotFoundUserException
+import com.restaurant.be.user.presentation.dto.GetMyUserResponse
+import com.restaurant.be.user.repository.UserRepository
+import org.springframework.stereotype.Service
+
+@Service
+class GetAccountService(
+    private val userRepository: UserRepository,
+) {
+    fun getMyUser(userId: Long): GetMyUserResponse {
+        val user =
+            userRepository.findById(userId).orElseThrow {
+                NotFoundUserException()
+            }
+
+        return GetMyUserResponse(user = user)
+    }
 }
--- src/main/kotlin/com/restaurant/be/user/presentation/controller/GetAccountController.kt
+++ src/main/kotlin/com/restaurant/be/user/presentation/controller/GetAccountController.kt
@@ -1,4 +1,28 @@
 package com.restaurant.be.user.presentation.controller
 
-class GetAccountController {
+import com.restaurant.be.user.domain.service.GetAccountService
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@Tag(name = "01. User Info", description = "유저 서비스")
+@RestController
+@RequestMapping("/v1/users")
+class GetAccountController(
+    private val getAccountService: GetAccountService,
+) {
+//    @GetMapping("/accounts")
+//    @PreAuthorize("hasRole('USER')")
+//    @Operation(summary = "계정정보조회 API")
+//    @ApiResponse(
+//        responseCode = "200",
+//        description = "성공",
+//        content = [Content(schema = Schema(implementation = GetMyUserResponse::class))],
+//    )
+//    fun getUserAccount(
+//        @RequestParam nickname: String
+//    ): CommonResponse<> {
+//        val response = getAccountService.getAccount(nickname)
+//        return CommonResponse.success(response)
+//    }
 }
