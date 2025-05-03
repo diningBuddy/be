e7103950c842458be47b0d0c48123ef0560f2fb1
xjwl0305 <xjwl0305@gmail.com>
2025-05-03 18:52:08 +0900

    UPDATE getAccountinfo Any -> SocialType? [KAN-331]

--- src/main/kotlin/com/restaurant/be/user/presentation/dto/GetAccountDto.kt
+++ src/main/kotlin/com/restaurant/be/user/presentation/dto/GetAccountDto.kt
@@ -4,6 +4,7 @@ package com.restaurant.be.user.presentation.dto
 
 import com.restaurant.be.user.domain.constant.Gender
 import com.restaurant.be.user.domain.constant.School
+import com.restaurant.be.user.domain.constant.SocialType
 import com.restaurant.be.user.domain.entity.User
 import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDate
@@ -15,7 +16,7 @@ data class GetAccountResponse(
     @Schema(title = "가입일", example = "2025-01-01", required = true)
     val createdAt: LocalDateTime,
     @Schema(title = "로그인유형", example = "카카오", required = true)
-    val loginType: Any,
+    val loginType: SocialType?,
     @Schema(title = "생일", example = "1997-03-05", required = true)
     val birthday: LocalDate,
     @Schema(title = "성별", example = "MAN", required = true)
@@ -28,10 +29,9 @@ data class GetAccountResponse(
     constructor(user: User) : this(
         id = user.id ?: 0,
         createdAt = user.createdAt,
-        loginType =  user.socialUsers
+        loginType = user.socialUsers
             .getOrNull(0)
-            ?.socialType
-            ?: "휴대폰", // 카카오, 애플 인증을 둘 다 활성화한 고객은 어떻게?
+            ?.socialType, // 카카오, 애플 인증을 둘 다 활성화한 고객은 어떻게?
         birthday = user.birthday,
         gender = user.gender,
         phoneNumber = user.phoneNumber,
