d1936c338562db182d84052fea6e1aa39a2f2da5
xjwl0305 <xjwl0305@gmail.com>
2025-05-03 23:11:56 +0900

    UPDATE getSocialUSerDto form [KAN-330]

--- src/main/kotlin/com/restaurant/be/user/presentation/dto/GetSocialUserDto.kt
+++ src/main/kotlin/com/restaurant/be/user/presentation/dto/GetSocialUserDto.kt
@@ -3,6 +3,7 @@
 package com.restaurant.be.user.presentation.dto
 
 import com.restaurant.be.user.domain.constant.SocialType
+import com.restaurant.be.user.domain.entity.SocialUser
 import com.restaurant.be.user.domain.entity.User
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.persistence.*
@@ -11,13 +12,19 @@ data class GetSocialUserDtoResponse(
     @Schema(title = "유저 아이디", example = "1", required = true)
     val id: Long?,
     val socialUsers: List<SocialUserDto>
-)
+){
+    constructor(user: User) : this(
+        id = user.id,
+        socialUsers = user.socialUsers
+            .map { SocialUserDto(it) }
+    )
+}
 
 data class SocialUserDto(
     @Schema(title = "소셜 유형", example = "KAKAO", required = true)
-    val socialType: SocialType?,
+    val socialType: SocialType,
 ) {
-    constructor(user: User) : this(
-        socialType = user.socialUsers.firstOrNull()?.socialType,
+    constructor(entity: SocialUser) : this(
+        socialType = entity.socialType
     )
 }
