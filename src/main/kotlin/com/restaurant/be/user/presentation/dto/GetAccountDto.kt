266237b2cf31ad96817f0e9343fd4133fd001037
xjwl0305 <xjwl0305@gmail.com>
2025-05-03 23:26:10 +0900

    UPDATE GetAccountResponse loginType form [KAN-331]

--- src/main/kotlin/com/restaurant/be/user/presentation/dto/GetAccountDto.kt
+++ src/main/kotlin/com/restaurant/be/user/presentation/dto/GetAccountDto.kt
@@ -16,7 +16,7 @@ data class GetAccountResponse(
     @Schema(title = "가입일", example = "2025-01-01", required = true)
     val createdAt: LocalDateTime,
     @Schema(title = "로그인유형", example = "카카오", required = true)
-    val loginType: SocialType?,
+    val loginType: List<SocialType>,
     @Schema(title = "생일", example = "1997-03-05", required = true)
     val birthday: LocalDate,
     @Schema(title = "성별", example = "MAN", required = true)
@@ -29,9 +29,9 @@ data class GetAccountResponse(
     constructor(user: User) : this(
         id = user.id ?: 0,
         createdAt = user.createdAt,
-        loginType = user.socialUsers
-            .getOrNull(0)
-            ?.socialType, // 카카오, 애플 인증을 둘 다 활성화한 고객은 어떻게?
+        loginType    = user.socialUsers
+            .map { it.socialType }
+            .toList(),
         birthday = user.birthday,
         gender = user.gender,
         phoneNumber = user.phoneNumber,
