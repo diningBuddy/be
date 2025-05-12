61b35f2ceb5fa97ec79d438cf8a89a5823b21f6c
Ting97ok <89396424+Ting97ok@users.noreply.github.com>
2025-05-12 21:14:49 +0900

    [Fix] @ModelAttribute 어노테이션 추가

--- src/main/kotlin/com/restaurant/be/recent/presentation/controller/RecentQueryController.kt
+++ src/main/kotlin/com/restaurant/be/recent/presentation/controller/RecentQueryController.kt
@@ -13,6 +13,7 @@ import jakarta.validation.Valid
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.security.Principal
@@ -46,7 +47,7 @@ class RecentQueryController(
     )
     fun deleteRecentQueries(
         principal: Principal,
-        @Valid
+        @Valid @ModelAttribute
         request: DeleteRecentQueriesRequest
     ): CommonResponse<Unit> {
         recentSearchService.deleteRecentQueries(principal.name.toLong(), request)
